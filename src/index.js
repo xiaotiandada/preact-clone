74fb2ceff24448c802e92a956b20aabf00b5f1f1
tianlei <qq952822399@gmail.com>
2024-06-25 10:27:47 +0800

    preact

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { render } from './render';
+export {
+	createElement,
+	createElement as h,
+	Fragment,
+	createRef,
+	isValidElement
+} from './create-element';
---
+++ src/render.js
@@ -0,0 +1,10 @@
+/**
+ * Render a Preact virtual node into a DOM element
+ * @param {ComponentChild} vnode The virtual node to render
+ * @param {PreactElement} parentDom The DOM element to render into
+ * @param {PreactElement | object} [replaceNode] Optional: Attempt to re-use an
+ * existing DOM tree rooted at `replaceNode`
+ */
+export function render(vnode, parentDom, replaceNode) {
+	
+}
